8f2b68b7dd38f583cbb8b211262e3f61d01de2a4
tathompson1123 <ty@thompsonsautodetailing.com>
2026-01-02 23:12:12 -0800

    Update HomePage.jsx

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1006,6 +1006,7 @@ const [isSigningUp, setIsSigningUp] = useState(false);
             </button>
           </div>
         </div>
+         )}
       {showSignupModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-2xl shadow-2xl max-w-md w-full p-8">
