1ffaeb530d20a61ba862191267420efc9175a9fc
tathompson1123 <ty@thompsonsautodetailing.com>
2026-01-30 13:31:56 -0800

    Update index.js

--- src/editor-v2/index.js
+++ src/editor-v2/index.js
@@ -1,1 +1,24 @@
+// ============================================
+// EDITOR V2 - MAIN EXPORTS
+// ============================================
 
+// Main Editor Component
+export { default as PageEditor } from './components/editor/PageEditor';
+
+// Schema utilities
+export {
+  generateId,
+  WIDGET_TYPES,
+  WIDGET_CATALOG,
+  SECTION_TEMPLATES,
+  createWidget,
+  createColumn,
+  createRow,
+  createSection,
+  createPage,
+  getDefaultWidgetContent,
+  getDefaultWidgetStyle,
+} from './utils/schema';
+
+// HTML Renderer
+export { renderPageToHtml } from './utils/htmlRenderer';
